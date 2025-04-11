b6c2600e70237243e975b34f0650e6add9c5c94d
Rida Tayyab <ridatayyab39@gmail.com>
2025-04-11 13:42:11 +0500

    Added new routes

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,20 +1,82 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import { v4 as uuidv4 } from "uuid";
 import cors from "cors";
-import helmet from "helmet";
-import bodyParser from "body-parser";
-import { z } from "zod";
-dotenv.config();
+import { createSessionStorage } from "react-router-dom";
 
 const app = express();
+const server = http.createServer(app);
+
+const PORT = 5000;
+
+type Session = {
+  meetingId: string;
+  participants: string[];
+};
+
+const sessions: Session[] = [];
+
 app.use(cors());
-app.use(bodyParser.json());
-app.use(helmet());
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
-app.use("/", require("./routes/indexRoute"));
-app.use("/api/login", require("./routes/loginRoute"));
+// Create a new meeting
+app.post("/create/meeting", (req, res) => {
+  const meetingId = uuidv4();
+  const username = req.body.name;
+  const userId = uuidv4() + "@" + username;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  sessions.push({
+    meetingId,
+    participants: [userId],
+  });
+
+  console.log(sessions[0].participants[0]);
+  console.log(sessions[0].meetingId);
+  res.json({ meetingId, userId });
 });
+
+// Join an existing meeting
+app.post("/join/meeting/:id", (req, res) => {
+  const { username } = req.body.name;
+  const meetingId = req.params.id;
+  const userId = uuidv4() + "@" + username;
+
+  console.log(sessions);
+  const session = sessions.find((s) => s.meetingId === meetingId);
+  if (!session) {
+    return res.status(404).json({ error: "Meeting not found" });
+  }
+
+  session.participants.push(userId);
+  res.json({ userId });
+});
+
+// Leave a meeting
+app.post("/leave/meeting", (req, res) => {
+  const { meetingId, userId } = req.body;
+
+  const sessionIndex = sessions.findIndex((s) => s.meetingId === meetingId);
+  if (sessionIndex === -1) {
+    return res.status(404).json({ error: "Meeting not found" });
+  }
+
+  const session = sessions[sessionIndex];
+  const userIndex = session.participants.indexOf(userId);
+  if (userIndex === -1) {
+    return res.status(404).json({ error: "User not found in the meeting" });
+  }
+
+  session.participants.splice(userIndex, 1);
+
+
+  // Delete session if empty
+  if (session.participants.length === 0) {
+    sessions.splice(sessionIndex, 1);
+  }
+
+  res.json({ message: "User left the meeting successfully" });
+});
+
+server.listen(PORT, () =>
+  console.log(`Primary server running on port ${PORT}`)
+);
