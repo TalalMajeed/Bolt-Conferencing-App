5f131d2ce3f8e32f0efd34cd2f803a45f055fa90
TalalMajeed <m.talal.majeed@gmail.com>
2025-07-08 18:16:13 +0500

    Updated Assets

--- Frontend-Web/src/app/layout.tsx
+++ Frontend-Web/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import favicon from "../../public/favicon.ico";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -17,6 +16,12 @@ export const metadata: Metadata = {
     title: "Bolt | Conferencing",
     description:
         "Connect seamlessly and collaborate effectively with Bolt video conferencing",
+    icons: [
+        {
+            rel: "icon",
+            url: "/favicon.png",
+        },
+    ],
 };
 
 export default function RootLayout({
@@ -26,10 +31,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <head>
-                <title>Bolt | Conferencing</title>
-                <link rel="icon" href="favicon" />
-            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
